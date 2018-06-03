5fe6151baf01147fd1c0ca06dc02138924e36761
PhantomZ89 <cshumtim@gmail.com>
2018-06-03 16:07:03 -0400

    Corrected some inconsistencies

--- Double_List_Driver.cpp
+++ Double_List_Driver.cpp
@@ -2,7 +2,7 @@
 #include <cstring>
 #include "Test.h"
 #include "Double_List_Test.h"
-#include "Double_node_test.h"
+#include "Double_Node_Test.h"
 
 int main( int argc, char *argv[] ) {
 	if ( argc > 2 ) {
@@ -18,11 +18,11 @@ int main( int argc, char *argv[] ) {
 			std::cerr << "Command-line argument must be either 'int' or 'double'. Got none, so using 'int' by default." << std::endl;
 		}
 
-		Double_List_Tester<int> tester;
+		Double_List_Test<int> tester;
 
 		tester.run();
 	} else if ( !std::strcmp( argv[1], "double" ) ) {
-		Double_List_tester<double> tester;
+		Double_List_Test<double> tester;
 
 		tester.run();
 	}
