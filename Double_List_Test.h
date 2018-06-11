24b49a19c3f6af27e9bda8b9a47c35b1b39b1d0b
PhantomZ89 <cshumtim@gmail.com>
2018-06-11 18:58:21 -0400

    Minor changes to test

--- Double_List_Test.h
+++ Double_List_Test.h
@@ -20,7 +20,7 @@ class Double_List_Test:public Tester< Double_Linked_List<Type> > {
 
 template <typename Type>
 Double_List_Test<Type>::Double_List_Test( Double_Linked_List<Type> *obj ):
-Tester< Double_Linked_List<Type> >( obj ) {
+test< Double_Linked_List<Type> >( obj ) {
 	// empty
 }
 
@@ -123,7 +123,7 @@ void Double_List_Test<Type>::process() {
 		} else {
 			std::cout << "Okay" << std::endl;
 
-			Double_Node_Test<Type> tester( actual_begin );
+			Double_Node_Test<Type> test( actual_begin );
 
 			tester.run();
 		}
